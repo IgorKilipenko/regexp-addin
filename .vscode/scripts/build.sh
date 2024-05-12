6f2c9e3929897ff15ab8e221f7c3e8789563436c
IgorKilipenko <igorkili@mail.ru>
2024-05-12 05:10:12 +0000

    add create Manifest

--- .vscode/scripts/build.sh
+++ .vscode/scripts/build.sh
@@ -1,66 +1,65 @@
 #!/usr/bin/env bash
 
+# Обработка аргументов
+if [ "$#" -eq 0 ]; then
+    target_dir="./target"
+else
+    target_dir="$1"
+fi
+
+if [ ! -d "$target_dir" ]; then
+    echo "Ошибка: '$target_dir' не является директорией или не существует"
+    exit 1
+fi
+
+#target_dir=$(cd "$target_dir" && pwd)
+
+# Сборка файлов
 cargo build --target x86_64-pc-windows-gnu
-#cargo build --target i686-pc-windows-gnu
+cargo build --target i686-pc-windows-gnu
 cargo build --target x86_64-unknown-linux-gnu
 #cargo build --target i686-unknown-linux-gnu
 
 # Создание архива
-
-if [ "$#" -eq 0 ]; then
-    files_path="./target"
-else
-    files_path="$1"
-fi
-
-if [ ! -d "$files_path" ]; then
-    echo "Ошибка: '$files_path' не является директорией или не существует"
-    exit 1
-fi
-
-cd "$files_path" || exit
-
 lib_name="regexp_addin"
 zipfile="$lib_name.zip"
 
-#zip -r "$zipfile" **/*"$lib_name".so ./**/*"$lib_name".dll
-#zip -r regexp_addin.zip . -i **/debug/*${lib_name}.so **/debug/*${lib_name}.dll
-#find . -type f \( -name "*${lib_name}.so" -o -name "*${lib_name}.dll" \) -path "*/debug/*" -not -path "*/debug/deps/*" -exec zip -j "$zipfile" {} +
+# Удаление существующего архива
+rm -f "$target_dir/$zipfile"
 
-# Используем find для поиска файлов .so и .dll в поддиректориях debug, игнорируя путь */debug/*deps
-# Передаем их в цикл for, где переименовываем файлы в зависимости от имен их родительских папок,
-# и упаковываем их с измененными именами в архив
-#find . -type f \( -name "*${lib_name}.so" -o -name "*${lib_name}.dll" \) -not -path "*/debug/*deps*" -print0 | while IFS= read -r -d '' file; do
-#    parent_dir="$(basename "$(dirname "$file")")"
-#    new_name="$parent_dir-$(basename "$file")"
-#    zip -r "$zipfile" "${file%/*}/$new_name"
-#done
-
-# Создаем временную директорию для копирования файлов
+# Создание временной директории для копирования файлов
 temp_dir=$(mktemp -d)
 
-# Объявляем ассоциативный массив для хранения уникальных имен файлов
+# Объявляение ассоциативный массив для хранения уникальных имен файлов
 declare -A unique_names
+
+cd "$target_dir" || exit
 
 # Используем find для поиска файлов .so и .dll в поддиректориях debug, игнорируя путь */debug/*deps
 # Передаем их в цикл for, где копируем файлы во временную директорию с измененными именами
 find . -type f \( -name "*${lib_name}.so" -o -name "*${lib_name}.dll" \) -not -path "*/debug/*deps*" -print0 | while IFS= read -r -d '' file; do
     file_name=$(basename "$file")
+    parent_dir=$(basename "$(dirname $(dirname "$file"))")
+    new_name="$parent_dir-$file_name"
 
     # Если имя файла уникально
-    if [[ -z ${unique_names[$file_name]} ]]; then
+    if [[ -z ${unique_names[$new_name]} ]]; then
         # Добавляем новое имя в ассоциативный массив
-        unique_names[$file_name]=1
-
-        parent_dir=$(basename "$(dirname "$file")")
-        new_name="$parent_dir-$file_name"
+        unique_names[$new_name]=1
 
         # Копируем файл во временную директорию с новым именем
         cp "$file" "$temp_dir/$new_name"
 
-        echo "Копирование файла $file_name в $temp_dir/$new_name"
+        #echo "Копирование файла $file_name в $temp_dir/$new_name"
     fi
 done
+
+echo '<?xml version="1.0" encoding="UTF-8"?>
+<bundle xmlns="http://v8.1c.ru/8.2/addin/bundle" name="ByteReader">
+    <component os="Windows" path="i686-pc-windows-gnu-regexp_addin.dll" type="native" arch="i386" />
+    <component os="Windows" path="x86_64-pc-windows-gnu-regexp_addin.dll" type="native" arch="x86_64" />
+    <component os="Linux" path="x86_64-unknown-linux-gnu-libregexp_addin.so" type="native" arch="x86_64" />
+</bundle>' > "$temp_dir/Manifest.xml"
 
 # Упаковываем скопированные файлы в архив
 zip -r -j "$zipfile" "$temp_dir"
@@ -68,4 +67,4 @@ zip -r -j "$zipfile" "$temp_dir"
 # Удаляем временную директорию
 rm -rf "$temp_dir"
 
-echo "Файлы из $files_path упакованы в $zipfile"
+echo "Файлы из $target_dir упакованы в $zipfile"
